ea99dbb5a9c7474fa0c883c3e9607ad09f21b3f6
vishnudk <vishnudk179@gmail.com>
2020-10-22 20:11:35 +0530

    changed the get request to post request

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+var bodyParser =require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var fs = require('fs')
+var fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var opn = require('opn');
-const { post } = require('./routes/index');
+// const { post } = require('./routes/index');
 var app = express();
+var urlParser = bodyParser.urlencoded({extended:false});
 // app.set('views', __dirname + '/views');
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'ejs');
@@ -19,17 +21,13 @@ app.get('/',function(req,res){
   console.log('server starte at http:\\127.0.0.1:9000')
   // res.send('hello world')
   // res.sendFile(path.join(__dirname,'login.html'));
-})
+});
 // app.post('/home',function(req,res){
 //   // res.sendFile(path.join(__dirname,'index.html'));
 //   console.log(req.body.passWrd)
   
 // })
-app.post('/post',function(req,res){
-  console.log("this is to show that post is working fine")
-  res.send("this is to just show that the error was due to the html filr not loading")
-  // res.sendFile(path.join(__dirname,'styledheets/login.html'));
-})
+
 app.get('/home',function(req,res) {
   res.sendFile(path.join(__dirname,'index.html'));
   console.log(__dirname)
@@ -41,18 +39,24 @@ app.get('/home',function(req,res) {
   }
   // console.log(req.body.fname)
   // console.log(req.body.lastName)
-})
+});
+app.post('/home',urlParser, function(req,res){
+  console.log(req.body)
+  // console.log("this is to show that post is working fine")
+  res.send("this is to just show that the error was due to the html filr not loading")
+  // res.sendFile(path.join(__dirname,'styledheets/login.html'));
+});
 app.get('/hello/:name',function(req,res){
   const name = req.params.name
   // res.sendFile('index.html');
   
   res.send("the name is "+name)
-})
+});
 app.get('/home/aums',function(req,res){
   // res.sendFile(path.join(__dirname,'styleSheets/test.html'))
   opn("https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F15%2F26%2F62%2F152662373b8c743a65c1ae9f42b8f8a2.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F825636544170514839%2F&tbnid=h3H5-RmtWxU9MM&vet=12ahUKEwi8yrP218fsAhUq1HMBHUX6DVcQMygDegUIARC8AQ..i&docid=Jr7IM2PJuu82qM&w=570&h=835&itg=1&q=minion&hl=en-US&ved=2ahUKEwi8yrP218fsAhUq1HMBHUX6DVcQMygDegUIARC8AQ")
   console.log("test")
-})
+});
 app.listen(9000,'0.0.0.0');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
